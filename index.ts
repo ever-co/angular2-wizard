cf306bc19882ecdbed1cc3a8e9400204e895db60
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-10-22 17:59:48 +0530

    fix: initial checking

--- index.ts
+++ index.ts
@@ -1,28 +1,17 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { WizardComponent } from './src/wizard.component';
-import { WizardStepComponent } from './src/wizard-step.component';
-
-export * from './src/wizard.component';
-export * from './src/wizard-step.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [
-    WizardComponent,
-    WizardStepComponent
-  ],
-  exports: [
-    WizardComponent,
-    WizardStepComponent
-  ]
+	imports: [
+		CommonModule
+	],
+	declarations: [ ],
+	exports: [ ]
 })
 export class FormWizardModule {
-  static forRoot(): ModuleWithProviders<FormWizardModule> {
-    return {
-      ngModule: FormWizardModule
-    };
-  }
+	static forRoot(): ModuleWithProviders<FormWizardModule> {
+		return {
+			ngModule: FormWizardModule
+		};
+	}
 }
